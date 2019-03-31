759bd73efe69311755fb32e387fb257071d3d47b
akGMIT <g00328529@gmit.ie>
2019-03-31 16:17:06 +0100

    regex.py function exec(regex, filename) implemented

--- regex.py
+++ regex.py
@@ -7,9 +7,15 @@ def main():
   parser.add_argument('filename', type=str, help='A file name containing strings')
 
   args = parser.parse_args()
+  exec(args.regex, args.filename)
 
-  print(args.regex)
-  print(args.filename)
+def exec(regex, filename):
+  f = open(filename, "r")
+
+  for line in f:
+    print(line.strip())
+    print(match.match(regex, line.strip()))
+    print()
 
 if __name__ == '__main__':
     main()
